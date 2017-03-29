4d4a4f2fbb0f90795fd31823e40f30e214602955
tomita-toshihiro <tomita@passeri.jp>
2017-03-30 03:27:04 +0900

    レスポンスをJSONに変更

--- webmecab.py
+++ webmecab.py
@@ -6,7 +6,21 @@ class MecabResource(object):
   def on_post(self, req, resp):
     body = req.stream.read().decode('utf-8')
     tagger = MeCab.Tagger()
-    resp.body = tagger.parse(body)
+
+    lines = []
+    for line in tagger.parse(body).split("\n"):
+      if line == "EOS" or line == "":
+        continue
+
+      e = line.split("\t")
+      word = e[0]
+      results = e[1].split(",")
+      lines.append({
+        "word": word,
+        "results": results
+      })
+
+    resp.body = json.dumps(lines)
 
 app = falcon.API()
 app.add_route("/", MecabResource())

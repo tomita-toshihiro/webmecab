c4fa0ce0ac31df170b26175bb1c28eb1bf23dff5
tomita-toshihiro <tomita@passeri.jp>
2017-03-29 23:31:47 +0900

    change hostname

--- webmecab.py
+++ webmecab.py
@@ -12,5 +12,5 @@ app.add_route("/", MecabResource())
 
 if __name__ == "__main__":
   from wsgiref import simple_server
-  httpd = simple_server.make_server("localhost", 8000, app)
+  httpd = simple_server.make_server('', 8000, app)
   httpd.serve_forever()

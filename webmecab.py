4f77ef6cb6a8f57f4810606ec29f2a4168eba63f
tomita-toshihiro <tomita@passeri.jp>
2017-03-29 23:08:11 +0900

    initial commit

---
+++ webmecab.py
@@ -0,0 +1,16 @@
+import json
+import falcon
+import MeCab
+
+class MecabResource(object):
+  def on_get(self, req, resp):
+    msg={"message": "hello, falcon"}
+    resp.body = json.dumps(msg)
+
+app = falcon.API()
+app.add_route("/", MecabResource())
+
+if __name__ == "__main__":
+  from wsgiref import simple_server
+  httpd = simple_server.make_server("localhost", 8000, app)
+  httpd.serve_forever()
